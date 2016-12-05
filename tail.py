4f4dd7845da592316f84efca55329b1ac6c7079f
Saad Zaher <eng.szaher@gmail.com>
2016-12-05 14:31:41 +0000

    Added python tail

---
+++ tail.py
@@ -0,0 +1,36 @@
+#
+#  Linux tail
+#
+#
+#
+
+import time
+
+
+def tail(filed):
+    """Python like tail generator. It takes a file descriptor and keeps for
+    waiting for data to arrive to the file. """
+    print "Started tailing ..."
+    # go to the end of the file
+    filed.seek(0, 2)
+    # keep looping to read from the file
+    while True:
+        # read some lines
+        _line = filed.readline()
+        # if there wasn't any new lines wait a little bit and do another loop
+        if not _line:
+            time.sleep(0.1)
+            continue
+        # send lines read from the log file
+        yield _line
+
+
+if __name__ == "__main__":
+    print "Started ... "
+    # open a file and get a file descriptor
+    filedesc = open("/var/log/apache2/access.log")
+    # read from the generator all newly arrived lines and print them to
+    # console.
+    for line in tail(filedesc):
+        print line,
+
